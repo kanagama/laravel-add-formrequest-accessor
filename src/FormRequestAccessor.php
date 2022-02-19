2a86bf5e100350c935d638a5d0a95478e2afc539
k-nagama <k.nagama0632@gmail.com>
2022-02-19 12:59:47 +0900

    [fix] バリデーションより先にアクセサが実行される不具合を修正

--- src/FormRequestAccessor.php
+++ src/FormRequestAccessor.php
@@ -7,22 +7,23 @@ use Illuminate\Support\Str;
 /**
  * FormRequest に accessor 機能を付与
  *
- * @method array $all($keys = null)
+ * @method void $passedValidation()
  *
  * @author k.nagama <k.nagama0632@gmail.com>
  */
 trait FormRequestAccessor
 {
     /**
-     * Illuminate\Http\Concerns\InteractsWithInput::all() を override
+     * Illuminate\Http\Concerns\InteractsWithInput::passedValidation() を override
      *
-     * @param  array|mixed|null  $keys
-     * @return array
+     * @return void
      *
      * @author k.nagama <k.nagama0632@gmail.com>
      */
-    public function all($keys = null): array
+    public function passedValidation(): void
     {
+        parent::passedValidation();
+
         $accessor = preg_grep('/^get.*Attribute/', get_class_methods(get_class())) ?? [];
         foreach ($accessor as $method) {
             preg_match('/(?<=get_).+(?=_attribute)/', str::snake($method), $match);
@@ -30,12 +31,9 @@ trait FormRequestAccessor
                 continue;
             }
 
-            // Request に追加
             $this->merge([
                 $match[0] => $this->{$method}(),
             ]);
         }
-
-        return parent::all($keys);
     }
 }
